b028ff47f5f99b3649d112e3ec9c396dab8c8ccd
devesh sharma <deveshsharma7618@gmail.com>
2026-02-11 18:04:39 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.6 MB (23.84%)

---
+++ 0114-flatten-binary-tree-to-linked-list/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+TreeNode* prev = NULL;
+    void flatten(TreeNode* root) {
+
+
+        if(root == NULL){
+            return;
+        }
+
+        flatten(root->right);
+        flatten(root->left);
+
+        root->left = NULL;
+        root->right = prev;
+        prev = root;
+    }
+};
