0728b8c6864b5fab2b740d4dbdc742d0fc3322a3
devesh sharma <deveshsharma7618@gmail.com>
2026-02-19 14:52:21 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 14 MB (83.96%)

---
+++ 0040-combination-sum-ii/solution.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void helper(vector<int>& candidates, int target, vector<vector<int>>& ans,
+                vector<int>& temp, int s, int i) {
+        if (s > target) return; 
+        if (s == target) {
+            ans.push_back(temp);
+            return;
+        }
+        for (int j = i; j < candidates.size(); j++) {
+            if (j > i && candidates[j] == candidates[j-1]) continue;
+            temp.push_back(candidates[j]);
+            helper(candidates, target, ans, temp, s + candidates[j], j+1); 
+            temp.pop_back();
+        }
+    }
+
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        vector<vector<int>> ans;
+        vector<int> temp;
+        helper(candidates, target, ans, temp, 0, 0);
+        return ans;
+    }
+};
