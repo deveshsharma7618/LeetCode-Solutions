038e7aa54a99bdee482adde037b783421ad8d3fb
devesh sharma <deveshsharma7618@gmail.com>
2026-07-16 17:47:59 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 113.6 MB (86.67%)

---
+++ 0643-maximum-average-subarray-i/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        int sum = 0;
+        for(int i =0;i< k; i++){
+            sum += nums[i];
+        }
+        
+        double x = sum/double(k);
+        for(int i=k;i<nums.size();i++){
+            
+            sum += nums[i];
+            sum -= nums[i-k];
+            
+            double temp = sum/double(k);
+            x = x > temp ? x : temp;
+            
+        }
+        
+        return x;
+    }
+};
