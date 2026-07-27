7299f2c596a83fd1ae76d5d93c5d12d4f057a40d
devesh sharma <deveshsharma7618@gmail.com>
2026-07-27 08:10:59 +0000

    [LeetCode Sync] Runtime - 3 ms (24.83%), Memory - 7.6 MB (94.78%)

---
+++ 1205-defanging-an-ip-address/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+void replaceAll(std::string& str, const std::string& from, const std::string& to) {
+    if (from.empty()) return;
+    size_t start_pos = 0;
+    while ((start_pos = str.find(from, start_pos)) != std::string::npos) {
+        str.replace(start_pos, from.length(), to);
+        start_pos += to.length(); // Move past the replaced substring to avoid infinite loops
+    }
+}
+
+    string defangIPaddr(string address) {
+        replaceAll(address, ".","[.]");
+        return address;
+    }
+};
