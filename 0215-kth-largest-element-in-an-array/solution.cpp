639ec60746e42c9a29fa2446d0326244142dc5ac
devesh sharma <deveshsharma7618@gmail.com>
2026-09-09 04:06:43 +0000

    [LeetCode Sync] Runtime - 49 ms (20.66%), Memory - 75.5 MB (17.36%)

---
+++ 0215-kth-largest-element-in-an-array/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int> pq;
+        for(auto x : nums){
+            pq.push(x);
+        }
+
+        int ans = -1;
+
+        for(int i = 0; i < k; i++){
+            ans = pq.top();
+            pq.pop();
+        }
+
+        return ans;
+
+    }
+};
