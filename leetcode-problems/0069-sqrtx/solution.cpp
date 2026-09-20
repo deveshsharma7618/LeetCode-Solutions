f02973cc48460ddd927aab9e33fe4d3ce3289364
devesh sharma <deveshsharma7618@gmail.com>
2026-09-20 22:53:56 +0530

    Delete leetcode-problems directory

--- leetcode-problems/0069-sqrtx/solution.cpp
+++
@@ -1,9 +0,0 @@
-class Solution {
-public:
-    int mySqrt(int x) {
-        long long ans = 0;
-        for(long long i=0;i*i <= (long long)x; i++)
-            ans = i;
-        return ans;
-    }
-};
