284109ef0e9ac42f5fec7e460a0b62b7c30e1fdf
devesh sharma <deveshsharma7618@gmail.com>
2026-09-20 17:00:18 +0000

    [LeetCode Sync] Runtime - 1 ms (27.83%), Memory - 11 MB (43.19%)

---
+++ 2195-time-needed-to-buy-tickets/solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int timeRequiredToBuy(vector<int>& tickets, int k) {
+        queue<int> q;
+        int n = tickets.size();
+        for(int i = 0; i < n; i++){
+            q.push(i);
+        }
+
+        int time = 0;
+
+        while(tickets[k] != 0){
+            tickets[q.front()]--;
+            time++;
+            if( tickets[q.front()] > 0)
+                q.push(q.front());
+
+            q.pop();
+        }
+
+        return time;
+
+    }
+};
